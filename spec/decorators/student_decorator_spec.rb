e49b6b302d6a414e9569cf935384c69f136735fe
Paweł Drożański <pawdro@gmail.com>
2016-11-09 20:35:00 +0100

    Tests to student.birthdate

--- spec/decorators/student_decorator_spec.rb
+++ spec/decorators/student_decorator_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe StudentDecorator do
   let(:teacher) { create :teacher, first_name: 'John', last_name: 'Smith' }
   let(:student) { create :student, first_name: 'John', last_name: 'Smith' }
+  let(:not_born_student) { create :student, birthdate: nil }
   let(:subject_item) { create :subject_item, teacher: teacher }
   let(:second_subject_item) { create :subject_item }
   let!(:note_1) { create :subject_item_note, value: 5, student: student, subject_item: second_subject_item }
@@ -27,6 +28,24 @@ describe StudentDecorator do
 
       it 'calculates avg of student notes' do
         is_expected.to eq '4.50'
+      end
+    end
+  end
+
+  describe '#formated_bithdate' do
+    describe 'when student has no birthday set' do
+      subject { not_born_student.decorate.formated_birthdate }
+
+      it 'returns nil' do
+        is_expected.to eq nil
+      end
+    end
+
+    describe 'when student has birthday set' do
+      subject { student.decorate.formated_birthdate }
+
+      it 'returns birthdate in format RRRR-MM_DD' do
+        is_expected.to match(/\d{4}\-\d{2}\_\d{2}/)
       end
     end
   end
--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :student do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    birthdate { Faker::Date.between(50.years.ago, Date.today) }
   end
 end
