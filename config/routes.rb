c5415d90f9260d2cdfb6f64ac2f88e146bd9ab58
Paweł Drożański <pawdro@gmail.com>
2016-11-09 14:59:14 +0100

    Add missing subjects routes within teachers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :students do
     get :subjects
   end
-  resources :teachers
-  get 'reports/subjects', to: 'reports#subjects'
+  resources :teachers do
+    get :subjects
+  end
+  get 'reports/subjects', to: 'reports#subjects', as: 'report_subjects'
   resources :visitors, only: [:index]
+  root 'teachers#index'
 end
