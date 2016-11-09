f31f6e5c1f2901b3d7efd2f51000a52084ab75da
Paweł Drożański <pawdro@gmail.com>
2016-11-09 16:03:20 +0100

    Validates password presence on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :registerable
   validates :email, presence: true, email: true
+  validates :password, presence: true
 end
